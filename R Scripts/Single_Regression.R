ec24b08b4468b48bf3bbba5f53976ef7edba99a5
healyt22 <healyt22@gmail.com>
2015-11-29 22:18:34 -0500

    committing ML to repo

---
+++ R Scripts/Single_Regression.R
@@ -0,0 +1,29 @@
+setwd("~/G_WD/R/machine-learning-ex1")
+
+dat1 = read.csv('ex1data1.txt')
+colnames(dat1) = c('x_1', 'y')
+dat1 = cbind(x_0 = 1, dat1)
+
+X = dat1[,1:2]
+y = dat1$y
+m = length(X)
+iterations = 1500
+alpha = 0.01
+theta = vector(mode = "numeric",length = 2)
+
+J = function(X, y, theta) {
+    h = X * theta
+    error = h - y
+    error_sqr = error^2
+    1 / (2 * m) * sum(error_sqr)
+}
+
+J_hist = vector()
+
+for( i in 1:iterations){
+    h = X * theta
+    errors = h - y
+    theta_change = (alpha / m) * t(X) * errors
+    theta = theta - theta_change
+    J_hist = append(J_hist, J(X, y, theta))
+}
