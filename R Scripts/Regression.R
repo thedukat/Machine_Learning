8e54170666e6051d00ee836f93b57924c11b930a
unknown <thealy@PC02LQ6R.se.memc.com>
2015-11-30 13:13:07 -0500

    still buggin

--- R Scripts/Regression.R
+++ R Scripts/Regression.R
@@ -5,30 +5,26 @@ if( Sys.info()['sysname'] == "Windows"){
 }
 
 
-## ----------------------------- Single Variable Regression ------------------------------##
+## ----------------------------- Single Variable Inputs -------------------------------------------
 
 dat1 = read.csv('ex1data1.txt')
 colnames(dat1) = c('x_1', 'y')
-dat1 = cbind(x_0 = 1, dat1)
 
 
-## -------------------------------- Multivariate Regression -------------------------------##
+## -------------------------------- Multivariate Inputs -------------------------------------------
 
 dat2 = read.csv('ex1data2.txt')
 colnames(dat2) = c("x_1", "x_2", "y")
 
-## Feature Normalization
-dat2$x_1 = (dat2$x_1 - mean(dat2$x_1) ) / sd(dat2$x_1)
-dat2$x_2 = (dat2$x_2 - mean(dat2$x_2) ) / sd(dat2$x_2)
-dat2 = cbind(x_0 = 1, dat2)
 
-
-## -------------------------------------- Function ----------------------------------------##
+## -------------------------------------- Linear Regression ---------------------------------------
 
 Regress = function(dat, input) {
     X = as.matrix(dat[,1:(ncol(dat) - 1)])
-    y = as.vector(dat$y)
-    m = length(X)
+    X = scale(X)
+    X = cbind(x_0 = 1, X)
+    y = dat$y
+    m = nrow(X)
     iterations = 1500
     alpha = 0.01
     theta = vector(mode = "numeric",length = ncol(X))
@@ -38,7 +34,7 @@ Regress = function(dat, input) {
         h = X %*% theta
         error = h - y
         error_sqr = error^2
-        1 / (2 * m) * sum(error_sqr)
+        J = (1 / (2 * m)) * sum(error_sqr)
     }
     
     J_hist = vector()
@@ -50,11 +46,17 @@ Regress = function(dat, input) {
         theta = theta - theta_change
         J_hist = append(J_hist, J(X, y, theta))
     }
+    
     plot(J_hist, type = "l")
-    paste(c("Theta found by gradient descent:"), theta )
+    h_theta = function(input) {
+        v = c(1, input)
+        calc = v %*% theta
+        calc
+    }
+    print(c("Thetas:", theta))
+    print(c("Result:", h_theta(input)))
     
-    v = c(1, input)
-    calc = v %*% theta
-    paste(c("Regression Result:", calc))
-    
-}
+} 
+
+Regress(dat1, 3.5)
+Regress(dat2, c(1650, 3))
