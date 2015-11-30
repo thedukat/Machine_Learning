f45d17a27d59fcce3671fb236fbca69b0ced17dc
unknown <thealy@PC02LQ6R.se.memc.com>
2015-11-30 11:11:07 -0500

    created function to dump data and regression inputs into

--- R Scripts/Regression.R
+++ R Scripts/Regression.R
@@ -11,35 +11,50 @@ dat1 = read.csv('ex1data1.txt')
 colnames(dat1) = c('x_1', 'y')
 dat1 = cbind(x_0 = 1, dat1)
 
-X = as.matrix(dat1[,1:2])
-y = as.vector(dat1$y)
-m = length(X)
-iterations = 1500
-alpha = 0.01
-theta = vector(mode = "numeric",length = 2)
 
-J = function(X, y, theta) {
-    h = X %*% theta
-    error = h - y
-    error_sqr = error^2
-    1 / (2 * m) * sum(error_sqr)
-}
+## -------------------------------- Multivariate Regression -------------------------------##
 
-J_hist = vector()
+dat2 = read.csv('ex1data2.txt')
+colnames(dat2) = c("x_1", "x_2", "y")
 
-for( i in 1:iterations){
-    h = X %*% theta
-    errors = h - y
-    theta_change = (alpha / m) * (t(X) %*% errors)
-    theta = theta - theta_change
-    J_hist = append(J_hist, J(X, y, theta))
-}
-plot(J_hist, type = "l")
-paste(c("Theta_0 found by gradient descent:"), theta[1] )
-paste(c("Theta_1 found by gradient descent:"), theta[2] )
+## Feature Normalization
+dat2$x_1 = (dat2$x_1 - mean(dat2$x_1) ) / sd(dat2$x_1)
+dat2$x_2 = (dat2$x_2 - mean(dat2$x_2) ) / sd(dat2$x_2)
+dat2 = cbind(x_0 = 1, dat2)
 
-predict = function(input) {
+
+## -------------------------------------- Function ----------------------------------------##
+
+Regress = function(dat, input) {
+    X = as.matrix(dat[,1:(ncol(dat) - 1)])
+    y = as.vector(dat$y)
+    m = length(X)
+    iterations = 1500
+    alpha = 0.01
+    theta = vector(mode = "numeric",length = ncol(X))
+    
+    
+    J = function(X, y, theta) {
+        h = X %*% theta
+        error = h - y
+        error_sqr = error^2
+        1 / (2 * m) * sum(error_sqr)
+    }
+    
+    J_hist = vector()
+    
+    for( i in 1:iterations){
+        h = X %*% theta
+        errors = h - y
+        theta_change = (alpha / m) * (t(X) %*% errors)
+        theta = theta - theta_change
+        J_hist = append(J_hist, J(X, y, theta))
+    }
+    plot(J_hist, type = "l")
+    paste(c("Theta found by gradient descent:"), theta )
+    
     v = c(1, input)
     calc = v %*% theta
-    calc * 10000
+    paste(c("Regression Result:", calc))
+    
 }
