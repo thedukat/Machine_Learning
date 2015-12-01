6fc27a5f85d5c20620cee39a079e893430fcf484
healyt22 <healyt22@gmail.com>
2015-11-30 23:21:48 -0500

    added single variable data

--- R Scripts/Regression2.R
+++ R Scripts/Regression2.R
@@ -4,6 +4,10 @@ if( Sys.info()['sysname'] == "Windows"){
     setwd("~/G_WD/Machine_Learning/R Scripts")
 }
 
+dat1 = read.csv('ex1data1.txt')
+colnames(dat1) = c('x_1', 'y')
+
+
 dat2 = read.csv('ex1data2.txt')
 colnames(dat2) = c("x_1", "x_2", "y")
 
@@ -30,4 +34,5 @@ Regression = function(dat, input) {
     print(c("Regression Result:", h))
 }
 
+Regression(dat1, 3.5)
 Regression(dat2, c(1650, 3))
